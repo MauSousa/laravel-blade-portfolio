1ec26b76113c81614e6c60aa6c97fb076bd23e99
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-09 21:31:43 -0600

    Add projects requests

---
+++ app/Http/Requests/Projects/StoreProjectRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Projects;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255', 'unique:projects,title'],
+            'description' => ['required', 'string', 'max:300'],
+            'technologies' => ['nullable', 'string', 'max:255'],
+            'repository_url' => ['nullable', 'url', 'max:255'],
+            'project_url' => ['nullable', 'url', 'max:255'],
+            'features' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+}
