2eeaa6ea55c76e774643ac04669ef0ee3d895e9e
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-09 21:31:25 -0600

    Add projects routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\Settings;
 use App\Models\Project;
 use Illuminate\Support\Facades\Route;
@@ -12,17 +13,24 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', '/settings/profile');
     Route::get('settings/profile', [Settings\ProfileController::class, 'edit'])->name('settings.profile.edit');
     Route::put('settings/profile', [Settings\ProfileController::class, 'update'])->name('settings.profile.update');
     Route::delete('settings/profile', [Settings\ProfileController::class, 'destroy'])->name('settings.profile.destroy');
     Route::get('settings/password', [Settings\PasswordController::class, 'edit'])->name('settings.password.edit');
     Route::put('settings/password', [Settings\PasswordController::class, 'update'])->name('settings.password.update');
     Route::get('settings/appearance', [Settings\AppearanceController::class, 'edit'])->name('settings.appearance.edit');
+});
+
+Route::prefix('projects')->name('project.')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [ProjectController::class, 'index'])->name('index');
+    Route::get('/create', [ProjectController::class, 'create'])->name('create');
+    Route::post('/', [ProjectController::class, 'store'])->name('store');
+    Route::get('/{project}', [ProjectController::class, 'show'])->name('show');
+    Route::get('/{project}/edit', [ProjectController::class, 'edit'])->name('edit');
+    Route::patch('/{project}', [ProjectController::class, 'update'])->name('update');
+    Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
 });
 
 require __DIR__ . '/auth.php';
