4d57ccef2d7b37e9c6b4c4779ac7a2d78c586345
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-09 21:35:29 -0600

    Initial changes for the page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,11 +23,13 @@
     @endif
 </head>
 
-<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<body
+    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6">
         {{-- <h1>Personal Portfolio</h1> --}}
     </header>
-    <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+    <div
+        class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main>
             <h1>Personal Portfolio</h1>
             @forelse ($projects as $project)
@@ -38,11 +40,13 @@
                             {{ $project->title }}
                         </h3>
                     </x-slot>
-                    <p>{{ $project->description }}</p>
+                    <p>{{ $project->description }}
+                    </p>
                     {{ $project->technologies }}
                     <section class="flex">
                         <a href="{{ $project->repository_url }}">
-                            Link to repository
+                            Link to
+                            repository
                         </a>
                         <a href="{{ $project->project_url }}">
                             Link to site
@@ -60,10 +64,12 @@
                 <x-slot name="header">
                     <h3
                         class="text-lg font-medium text-gray-800 dark:text-gray-200">
-                        Card with Header</h3>
+                        Card with Header
+                    </h3>
                 </x-slot>
 
-                <p class="text-gray-600 dark:text-gray-400">Card content goes
+                <p class="text-gray-600 dark:text-gray-400">
+                    Card content goes
                     here.</p>
 
                 <x-slot name="footer">
