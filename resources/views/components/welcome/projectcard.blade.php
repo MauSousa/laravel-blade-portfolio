a15ac625a4dae74ec02643a9925797bbe599ac4c
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-20 18:53:26 -0600

    Apply formatter

--- resources/views/components/welcome/projectcard.blade.php
+++ resources/views/components/welcome/projectcard.blade.php
@@ -1,4 +1,11 @@
-@props(['title', 'description', 'tech', 'repository_url', 'project_url', 'features'])
+@props([
+    'title',
+    'description',
+    'tech',
+    'repository_url',
+    'project_url',
+    'features',
+])
 
 <div
     class="bg-gray-50 dark:bg-dark200 p-6 rounded-xl shadow-md hover:shadow-lg transition-shadow">
@@ -10,31 +17,31 @@
     </p>
 
     <!-- Features -->
-        <div class="grid grid-cols-1 gap-2 mb-4">
-            <p>Some features</p>
+    <div class="grid grid-cols-1 gap-2 mb-4">
+        <p>Some features</p>
         <div class="gap-2 flex flex-wrap">
-        @forelse ($features as $feature)
-            <span
-                class="bg-blue-100 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1 rounded text-sm">
-                {{ $feature }}
-            </span>
-        @empty
-        @endforelse
+            @forelse ($features as $feature)
+                <span
+                    class="bg-blue-100 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1 rounded text-sm">
+                    {{ $feature }}
+                </span>
+            @empty
+            @endforelse
         </div>
     </div>
 
     <!-- Tecnologies -->
-        <div class="grid grid-cols-1 gap-2 mb-4">
-            <p>Tecnologies used</p>
-    <div class="flex flex-wrap gap-2 mb-4">
-        @forelse ($tech as $project_tech)
-            <span
-                class="bg-blue-100 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1 rounded text-sm">
-                {{ $project_tech }}
-            </span>
-        @empty
-        @endforelse
-    </div>
+    <div class="grid grid-cols-1 gap-2 mb-4">
+        <p>Tecnologies used</p>
+        <div class="flex flex-wrap gap-2 mb-4">
+            @forelse ($tech as $project_tech)
+                <span
+                    class="bg-blue-100 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 px-3 py-1 rounded text-sm">
+                    {{ $project_tech }}
+                </span>
+            @empty
+            @endforelse
+        </div>
     </div>
 
     <!-- Repository url -->
@@ -43,7 +50,8 @@
             <a href={{ $repository_url }}
                 class="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 transition-colors flex gap-x-2">
                 <svg width="25" height="25" viewBox="0 0 1024 1024"
-                    class="fill-none dark:fill-gray-200" xmlns="http://www.w3.org/2000/svg">
+                    class="fill-none dark:fill-gray-200"
+                    xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
                         transform="scale(64)" />
@@ -56,8 +64,9 @@
             <a href={{ $project_url }}
                 class="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300 transition-colors flex gap-x-2">
                 <!-- License: CC Attribution. Made by zwicon: https://www.zwicon.com/ -->
-                <svg class="fill-gray-900 dark:fill-gray-50" width="25px" height="25px"
-                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg class="fill-gray-900 dark:fill-gray-50" width="25px"
+                    height="25px" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M3.51211712,15 L8.17190229,15 C8.05949197,14.0523506 8,13.0444554 8,12 C8,10.9555446 8.05949197,9.94764942 8.17190229,9 L3.51211712,9 C3.18046266,9.93833678 3,10.9480937 3,12 C3,13.0519063 3.18046266,14.0616632 3.51211712,15 L3.51211712,15 Z M3.93551965,16 C5.12590433,18.3953444 7.35207678,20.1851177 10.0280093,20.783292 C9.24889451,19.7227751 8.65216136,18.0371362 8.31375067,16 L3.93551965,16 L3.93551965,16 Z M20.4878829,15 C20.8195373,14.0616632 21,13.0519063 21,12 C21,10.9480937 20.8195373,9.93833678 20.4878829,9 L15.8280977,9 C15.940508,9.94764942 16,10.9555446 16,12 C16,13.0444554 15.940508,14.0523506 15.8280977,15 L20.4878829,15 L20.4878829,15 Z M20.0644804,16 L15.6862493,16 C15.3478386,18.0371362 14.7511055,19.7227751 13.9719907,20.783292 C16.6479232,20.1851177 18.8740957,18.3953444 20.0644804,16 L20.0644804,16 Z M9.18440269,15 L14.8155973,15 C14.9340177,14.0623882 15,13.0528256 15,12 C15,10.9471744 14.9340177,9.93761183 14.8155973,9 L9.18440269,9 C9.06598229,9.93761183 9,10.9471744 9,12 C9,13.0528256 9.06598229,14.0623882 9.18440269,15 L9.18440269,15 Z M9.3349823,16 C9.85717082,18.9678295 10.9180729,21 12,21 C13.0819271,21 14.1428292,18.9678295 14.6650177,16 L9.3349823,16 L9.3349823,16 Z M3.93551965,8 L8.31375067,8 C8.65216136,5.96286383 9.24889451,4.27722486 10.0280093,3.21670804 C7.35207678,3.81488234 5.12590433,5.60465556 3.93551965,8 L3.93551965,8 Z M20.0644804,8 C18.8740957,5.60465556 16.6479232,3.81488234 13.9719907,3.21670804 C14.7511055,4.27722486 15.3478386,5.96286383 15.6862493,8 L20.0644804,8 L20.0644804,8 Z M9.3349823,8 L14.6650177,8 C14.1428292,5.03217048 13.0819271,3 12,3 C10.9180729,3 9.85717082,5.03217048 9.3349823,8 L9.3349823,8 Z M12,22 C6.4771525,22 2,17.5228475 2,12 C2,6.4771525 6.4771525,2 12,2 C17.5228475,2 22,6.4771525 22,12 C22,17.5228475 17.5228475,22 12,22 Z" />
                 </svg>
