11a0194fea5a4d28278b2e82f7de14e329b11698
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-23 16:23:52 -0600

    Change font size

--- resources/views/components/status-session-badge.blade.php
+++ resources/views/components/status-session-badge.blade.php
@@ -2,7 +2,7 @@
 
 @if ($status)
     <div
-        {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600 dark:text-green-400']) }}>
+        {{ $attributes->merge(['class' => 'font-medium text-xl text-green-600 dark:text-gray-200 dark:bg-cyan-700']) }}>
         {{ $status }}
     </div>
 @endif
