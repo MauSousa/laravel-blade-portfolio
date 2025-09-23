1f895b707d23072792104061a18ffd7afd34286d
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-23 16:24:02 -0600

    Add name props

--- resources/views/components/welcome/link.blade.php
+++ resources/views/components/welcome/link.blade.php
@@ -1,4 +1,4 @@
-@props(['href' => '#'])
+@props(['href' => '#', 'name'])
 
 <a href="{{ $href }}"
-    class="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors">{{ $slot }}</a>
+    class="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors">{{ $name ?? $slot }}</a>
