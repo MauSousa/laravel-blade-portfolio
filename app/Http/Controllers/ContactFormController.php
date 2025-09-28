268af47dc9879fe35eec0cc6ebdb2e49b3c9989d
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-27 20:04:20 -0600

    Add logic to send mail

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactFormRequest;
+use App\Mail\ContactForm;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactFormController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(ContactFormRequest $request): RedirectResponse
     {
-        dd($request->all());
+        $mail = config('mail.from')['address'];
+
+        Mail::to($mail)->queue(new ContactForm($request->validated()));
+
+        return to_route('home')->with(['email_status' => 'Thank you for contacting me. I will get in touch with you shortly.']);
     }
 }
