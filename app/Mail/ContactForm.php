bcb47edf25b4b364040eb4f7f6b88cf5266a4e2a
Mauricio Sousa <lagrange.tesla@gmail.com>
2025-09-28 13:20:23 -0600

    Add mail contact form

---
+++ app/Mail/ContactForm.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactForm extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  array<string,mixed>  $data
+     */
+    public function __construct(public array $data)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            replyTo: [new Address($this->data['email'], $this->data['name'])],
+            subject: 'Contact Form',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.contact',
+            with: [
+                'contactMessage' => $this->data['message'],
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/mail/contact.blade.php
@@ -0,0 +1,1 @@
+<h1>{{ $contactMessage }}</h1>
